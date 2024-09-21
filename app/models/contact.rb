a61c10d9a59f7be30cac26b8440740ff41b23e6d
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-09-21 03:11:15 -0300

    Render Json Association Data

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,7 +4,11 @@ class Contact < ApplicationRecord
         "Test"
     end
 
+    def kind_description
+        self.kind.description
+    end
+
     def as_json(options={})
-        super(root: true, methods: :author)
+        super(root: true, methods: [:kind_description, :author])
     end
 end
