53e74eac15224be3c6c1c96c5c4ac80ac63bec63
Lucas Freymund <lucasehresmann.osik@gmail.com>
2025-10-08 02:52:08 -0300

    Mime Types Limiters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+    before_action :ensure_json_request
+    
+    # application/vnd.api+json
+    def ensure_json_request
+        return if request.headers["Accept"] =~ /vnd\.api\+json/
+        render :nothing => true, :status => 406
+    end
 end
