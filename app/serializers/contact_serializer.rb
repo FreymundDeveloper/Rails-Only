db6e59dd3f6a92d061e0380e43ccfe47878eb209
Lucas Freymund <lucasehresmann.osik@gmail.com>
2025-10-08 02:24:08 -0300

    Features Hateoas and Meta

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,14 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
-  belongs_to :kind
+  belongs_to :kind do 
+    link(:related) { contact_path(object.kind.id) }
+  end
+
   has_many :phones
   has_one :address
+
+  #link(:self) { contact_url(object.id) }
 
   def attributes(*args)
     h = super(*args)
@@ -11,4 +16,8 @@ class ContactSerializer < ActiveModel::Serializer
     h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
     h
   end
+
+  #meta do
+  #  { author: "FD" }
+  #end
 end
