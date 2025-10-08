bd213e1a30b61ee732908ad1e2fd6b2220879056
Lucas Freymund <lucasehresmann.osik@gmail.com>
2025-10-08 20:37:12 -0300

    Digest Authenticate

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -1,4 +1,11 @@
 class KindsController < ApplicationController
+  # include ActionController::HttpAuthentication::Basic::ControllerMethods
+  # http_basic_authenticate_with name: "fd", password: "secret"
+
+  include ActionController::HttpAuthentication::Digest::ControllerMethods
+  USERS = { "fd" => Digest::MD5.hexdigest(["fd","Application","secret"].join(":")) }
+
+  before_action :authenticate
   before_action :set_kind, only: %i[ show update destroy ]
 
   # GET /kinds
@@ -52,5 +59,11 @@ class KindsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def kind_params
       params.require(:kind).permit(:description)
+    end
+
+    def authenticate 
+      authenticate_or_request_with_http_digest("Application") do |username|
+        USERS[username]
+      end
     end
 end
